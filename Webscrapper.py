51f6564525e28d2a82466403ef40171d6a61a7c4
7blink <7blinkcreations@gmail.com>
2016-05-26 13:57:20 -0400

    added webscraper demo

--- Webscrapper.py
+++ Webscrapper.py
@@ -1,40 +1,37 @@
 import requests
 
-alphanumString = "abcdefghijklmnopqrstuvwkyz1234567890a"
+alphanumString = "abcdefghijklmnopqrstuvwkyz1234567890"
 alphanum = [c for c in alphanumString]
 
-git = "http://www.github.com/"
+#add website name
+website = ""
+git = "http://www." + website + ".com/"
 
-start = 99
+start = 0
 end = 37000
 
 def compute():
-	#request = requests.get('http://www.github.com/')
 
 	for i in range(start,end):
 
-		#print(getChar(i))
 		r = git + getChar(i)
 		request = requests.get(r)
-		#print(request.status_code)
 		if(404==request.status_code):
 			print(getChar(i))
-		#if(getChar(i) == "aa"):
-		#	print(getChar(i))
-	#print(request.status_code)
 
+#takes a number and returns a alpha numeric number
 def getChar(num):
 	tempNum = num
 	repeat = 1
 	length = len(alphanum)
-	while(tempNum>length-1):
+	while(tempNum>length):
 		tempNum = tempNum/length
 		repeat = repeat+1
 
 	tempString = ""
 	tempNum = num
 	while(repeat>0):
-		tempString = tempString + alphanum[tempNum%length]
+		tempString = tempString + alphanum[(tempNum%(length+1)-1)]
 		tempNum = tempNum/length
 		repeat = repeat-1
 
